d3d89b932fba6789c2f7f045c86c6d8b30786fff
Cobancho <tomycastelli@gmail.com>
2024-09-24 01:23:37 -0300

    Sacando los dist

--- packages/adapters/dist/index.js
+++
@@ -1,4 +0,0 @@
-export { CoinGecko } from "./providers/coins.provider";
-export { CoinsPostgres } from "./repositories/coins.repository";
-export { WalletsProviderAdapters } from "./providers/wallets.provider";
-export { WalletsPostgres } from "./repositories/wallets.repository";
