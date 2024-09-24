1c000d3377a4040150c97e772dfedc331608dcbf
Cobancho <tomycastelli@gmail.com>
2024-09-24 01:23:02 -0300

    Arreglos al empezar a probar el servicio de wallet

--- packages/domain/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { CoinsService } from "./core/coins.service";
-export { WalletsService } from "./core/wallets.service";
-export type { CoinsProvider, CoinsRepository } from "./core/coins.ports";
-export type { WalletsProvider, WalletsRepository } from "./core/wallets.ports";
-export type { Candle, Coin, CoinMarketData, SavedCoin, NFT, SavedNFT, } from "./core/coins.entities";
-export type { CoinedTransaction, CoinedWallet, CoinedWalletCoin, CoinedWalletWithTransactions, Transaction, ValuedWallet, ValuedWalletCoin, Wallet, } from "./core/wallets.entities";
-export * from "./core/vars";
-//# sourceMappingURL=index.d.ts.map
--- packages/domain/dist/index.js
+++
@@ -1,3 +0,0 @@
-export { CoinsService } from "./core/coins.service";
-export { WalletsService } from "./core/wallets.service";
-export * from "./core/vars";
